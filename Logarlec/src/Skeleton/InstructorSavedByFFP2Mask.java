f364ca5ffeade532ff6cd85490791a7c413a90c9
Domonkos <46531765+Frenetiqus@users.noreply.github.com>
2024-03-20 19:33:55 +0100

    példa egy tesztre

---
+++ Logarlec/src/Skeleton/InstructorSavedByFFP2Mask.java
@@ -0,0 +1,21 @@
+package Skeleton;
+
+import modul.DoorSide;
+import modul.FFP2Mask;
+import modul.Instructor;
+import modul.Room;
+
+public class InstructorSavedByFFP2Mask {
+    private Instructor i;
+    private Room r1, r2;
+    private DoorSide d1, d2;
+    private FFP2Mask f;
+
+    private void InitTest(){
+        // Fuggvenyhivasok a kommunikacios diagrammon
+
+    }
+    public static void Run(){
+        // Fuggvenyhivasok a szekvencia diagrammon
+    }
+}
--- Logarlec/src/Skeleton/SkeletonMain.java
+++ Logarlec/src/Skeleton/SkeletonMain.java
@@ -4,5 +4,13 @@ import modul.*;
 
 public class SkeletonMain {
     public static void main(String[] args) {
+        // Tesztek listazasa
+        System.out.println("1. Oktatót megvédi FFP2 maszk");
+        // ..
+
+        // input: 1
+        // switch
+        int input = 1;
+        if(input == 1) InstructorSavedByFFP2Mask.Run();
     }
 }
