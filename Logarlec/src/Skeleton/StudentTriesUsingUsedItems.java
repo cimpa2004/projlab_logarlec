6c2f39eb101912e8c8bce22c2b582bbf2998adba
Domonkos <46531765+Frenetiqus@users.noreply.github.com>
2024-03-21 20:00:18 +0100

    merged main

---
+++ Logarlec/src/Skeleton/StudentTriesUsingUsedItems.java
@@ -0,0 +1,23 @@
+package Skeleton;
+
+import modul.DoorSide;
+import modul.FFP2Mask;
+import modul.Instructor;
+import modul.Room;
+
+public class StudentTriesUsingUsedItems {
+
+    private static void initTest(){
+
+    }
+
+    public static void Run(){
+        System.out.println("Init");
+        initTest();
+        System.out.println("--------");
+        System.out.println("Run");
+
+
+        System.out.println("--------");
+    }
+}
