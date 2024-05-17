66f7a8660210c95eba7f0306324d53a0e503863e
LestyanAdam <144900051+LestyanAdam@users.noreply.github.com>
2024-05-17 19:34:15 +0200

    Update VFFP2Mask.java

--- Logarlec/src/view/VFFP2Mask.java
+++ Logarlec/src/view/VFFP2Mask.java
@@ -1,5 +1,6 @@
 package view;
 
+import util.Logger;
 import viewmodel.IVFFP2Mask;
 
 import javax.swing.*;
@@ -7,7 +8,7 @@ import java.awt.*;
 
 public class VFFP2Mask extends VItem{
     IVFFP2Mask ivFFP2Mask;
-    
+    VStudent owner;
     public VFFP2Mask(IVFFP2Mask ivffp2Mask){
         this.ivFFP2Mask = ivffp2Mask;
         this.ivFFP2Mask.SetIVItemUpdate(this);
@@ -18,7 +19,9 @@ public class VFFP2Mask extends VItem{
      */
     @Override
     public void PickedUp() {
-
+        Logger.startedView(this, "PickedUp");
+        Logger.finishedView(this, "PickedUp");
+        owner.input.PickupItem(owner.GetID(), ivFFP2Mask);
     }
 
     /**
@@ -26,6 +29,8 @@ public class VFFP2Mask extends VItem{
      */
     @Override
     public CirclePanel DrawOnMap() {
+        Logger.startedView(this, "DrawOnMap");
+        Logger.finishedView(this, "DrawOnMap");
         return new CirclePanel(new Color(255,255,255));
     }
 
@@ -33,21 +38,11 @@ public class VFFP2Mask extends VItem{
      * @param panel
      */
     @Override
-    public void DrawInInventory(JPanel panel) {
-        JPanel itemPanel = new JPanel();
-        itemPanel.setPreferredSize(new Dimension(100, 50)); // Set preferred size
-        itemPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK)); // Add border for visualization
-
-        BoxLayout boxlayout = new BoxLayout(itemPanel, BoxLayout.Y_AXIS);
-        itemPanel.setLayout(boxlayout);
-
-        CirclePanel invCirclePanel = new CirclePanel(new Color(255,255,255));
-        JButton useButton = new JButton();
-        JButton throwButton = new JButton();
-
-        itemPanel.add(invCirclePanel);
-        itemPanel.add(useButton);
-        itemPanel.add(throwButton);
+    public void DrawInInventory(JPanel panel, VStudent student) {
+        Logger.startedView(this, "DrawInInventory", panel, student);
+        Logger.finishedView(this, "DrawInInventory", panel, student);
+        owner = student;
+        InventoryItemPanel itemPanel = new InventoryItemPanel(new Color(255,255,255), true, false, this);
 
         panel.add(itemPanel);
     }
@@ -57,6 +52,8 @@ public class VFFP2Mask extends VItem{
      */
     @Override
     public boolean HasNullable() {
+        Logger.startedView(this, "HasNullable");
+        Logger.finishedView(this, "HasNullable");
         return false;
     }
 
@@ -64,7 +61,47 @@ public class VFFP2Mask extends VItem{
      *
      */
     @Override
-    public void Throw() {
+    public void Used() {
+        Logger.startedView(this, "Used");
+        Logger.finishedView(this, "Used");
+        owner.input.UseItem(owner.GetID(), ivFFP2Mask);
+    }
 
+    /**
+     *
+     */
+    @Override
+    public void Thrown() {
+        Logger.startedView(this, "Thrown");
+        Logger.finishedView(this, "Thrown");
+        owner.input.ThrowItem(owner.GetID(), ivFFP2Mask);
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void Connected() {
+        Logger.startedView(this, "Connected");
+        Logger.finishedView(this, "Connected");
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public VTransistor GetClickedT() {
+        Logger.startedView(this, "GetClickedT");
+        Logger.finishedView(this, "GetClickedT");
+        return null;
+    }
+
+    /**
+     * @param t
+     */
+    @Override
+    public void SetClickedT(VTransistor t) {
+        Logger.startedView(this, "SetClickedT", t);
+        Logger.finishedView(this, "SetClickedT", t);
     }
 }
