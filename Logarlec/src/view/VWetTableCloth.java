22c581ffd89d832d9837e5dbc2850ece9d560526
LestyanAdam <144900051+LestyanAdam@users.noreply.github.com>
2024-05-17 19:37:55 +0200

    Update VWetTableCloth.java

--- Logarlec/src/view/VWetTableCloth.java
+++ Logarlec/src/view/VWetTableCloth.java
@@ -1,5 +1,6 @@
 package view;
 
+import util.Logger;
 import viewmodel.IVWetTableCloth;
 
 import javax.swing.*;
@@ -7,6 +8,7 @@ import java.awt.*;
 
 public class VWetTableCloth extends VItem {
     IVWetTableCloth ivWetTableCloth;
+    VStudent owner;
 
     public VWetTableCloth(IVWetTableCloth ivWetTableCloth) {
         this.ivWetTableCloth = ivWetTableCloth;
@@ -18,7 +20,9 @@ public class VWetTableCloth extends VItem {
      */
     @Override
     public void PickedUp() {
-
+        Logger.startedView(this, "PickedUp");
+        Logger.finishedView(this, "PickedUp");
+        owner.input.PickupItem(owner.GetID(), ivWetTableCloth);
     }
 
     /**
@@ -26,6 +30,8 @@ public class VWetTableCloth extends VItem {
      */
     @Override
     public CirclePanel DrawOnMap() {
+        Logger.startedView(this, "DrawOnMap");
+        Logger.finishedView(this, "DrawOnMap");
         return new CirclePanel(new Color(153,0,153));
     }
 
@@ -33,21 +39,11 @@ public class VWetTableCloth extends VItem {
      * @param panel
      */
     @Override
-    public void DrawInInventory(JPanel panel) {
-        JPanel itemPanel = new JPanel();
-        itemPanel.setPreferredSize(new Dimension(100, 50)); // Set preferred size
-        itemPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK)); // Add border for visualization
-
-        BoxLayout boxlayout = new BoxLayout(itemPanel, BoxLayout.Y_AXIS);
-        itemPanel.setLayout(boxlayout);
-
-        CirclePanel circlePanel = new CirclePanel(new Color(153,0,153));
-        JButton useButton = new JButton();
-        JButton throwButton = new JButton();
-
-        itemPanel.add(circlePanel);
-        itemPanel.add(useButton);
-        itemPanel.add(throwButton);
+    public void DrawInInventory(JPanel panel, VStudent student) {
+        Logger.startedView(this, "DrawInInventory", panel, student);
+        Logger.finishedView(this, "DrawInInventory", panel, student);
+        owner = student;
+        InventoryItemPanel itemPanel = new InventoryItemPanel(new Color(153,0,153), true, false, this);
 
         panel.add(itemPanel);
     }
@@ -57,6 +53,8 @@ public class VWetTableCloth extends VItem {
      */
     @Override
     public boolean HasNullable() {
+        Logger.startedView(this, "HasNullable");
+        Logger.finishedView(this, "HasNullable");
         return false;
     }
 
@@ -64,12 +62,47 @@ public class VWetTableCloth extends VItem {
      *
      */
     @Override
-    public void Throw() {
-
+    public void Used() {
+        Logger.startedView(this, "Used");
+        Logger.finishedView(this, "Used");
+        owner.input.UseItem(owner.GetID(), ivWetTableCloth);
     }
 
+    /**
+     *
+     */
     @Override
-    public void UsedUpdate() {
+    public void Thrown() {
+        Logger.startedView(this, "Thrown");
+        Logger.finishedView(this, "Thrown");
+        owner.input.ThrowItem(owner.GetID(), ivWetTableCloth);
+    }
 
+    /**
+     *
+     */
+    @Override
+    public void Connected() {
+        Logger.startedView(this, "Connected");
+        Logger.finishedView(this, "Connected");
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public VTransistor GetClickedT() {
+        Logger.startedView(this, "GetClickedT");
+        Logger.finishedView(this, "GetClickedT");
+        return null;
+    }
+
+    /**
+     * @param t
+     */
+    @Override
+    public void SetClickedT(VTransistor t) {
+        Logger.startedView(this, "SetClickedT", t);
+        Logger.finishedView(this, "SetClickedT", t);
     }
 }
